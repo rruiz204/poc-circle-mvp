c0bdce8ae232983b40cac860a74954eeaabca467
roney ruiz <roneyruizrojas@gmail.com>
2025-05-10 23:15:10 -0600

    feat(server): create role model and define roles

---
+++ server/src/Domain/Models/Role.ts
@@ -0,0 +1,15 @@
+import { Prisma } from "generated/prisma";
+import type { Role as Model } from "generated/prisma";
+
+export namespace Role {
+  export type Entity = Model;
+  export type Records = Model[];
+  export type Nullable = Model | null;
+  export type Optional = Partial<Model>;
+
+  export type CreateParams = Prisma.RoleCreateInput;
+  export type UpdateParams = Prisma.RoleUpdateInput;
+
+  export type WhereParams = Prisma.RoleWhereInput;
+  export type OrderParams = Prisma.RoleOrderByWithRelationInput;
+};
