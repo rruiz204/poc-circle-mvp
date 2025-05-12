53f29102a3dc4067c759195c40f4f11771454f9e
roney ruiz <roneyruizrojas@gmail.com>
2025-05-11 23:24:21 -0600

    chore(server): rename authentication middleware

--- server/src/Presentation/Middlewares/AthenticationMiddleware.ts
+++ server/src/Presentation/Middlewares/AthenticationMiddleware.ts
@@ -2,12 +2,13 @@ import { JwtService } from "@Services/Tokens/JwtService";
 import { HttpException } from "@Exceptions/HttpException";
 import type { Request, Response, NextFunction } from "express";
 
-export const AuthMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+export const AuthenticationMiddleware = async (req: Request, res: Response, next: NextFunction) => {
   const token = req.header("Authorization")?.replace("Bearer ", "");
   if (!token) throw new HttpException.Unauthorized("Invalid token");
 
   try {
-    req.user.token = await JwtService.verify(token);
+    const payload = await JwtService.verify(token);
+    req.user.id = payload.id;
     next();
   } catch {
     throw new HttpException.Unauthorized("Bad token");
--- server/src/Presentation/Types/Express.d.ts
+++ server/src/Presentation/Types/Express.d.ts
@@ -4,7 +4,7 @@ declare global {
   namespace Express {
     interface Request {
       user: {
-        token?: JwtPayload;
+        id: number;
       };
     };
   };
