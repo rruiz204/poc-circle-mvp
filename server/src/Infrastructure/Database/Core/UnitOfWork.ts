4bebbd2960dadea87dae05bca1f13578ac898012
roney ruiz <roneyruizrojas@gmail.com>
2025-05-13 20:58:22 -0600

    feat(server): add repositories to uow

--- server/src/Infrastructure/Database/Core/UnitOfWork.ts
+++ server/src/Infrastructure/Database/Core/UnitOfWork.ts
@@ -2,12 +2,18 @@ import { PrismaClient } from "generated/prisma";
 import { injectable } from "inversify";
 
 import { UserRepository } from "@Repositories/UserRepository";
+import { RoleRepository } from "@Repositories/RoleRepository";
+import { ScopeRepository } from "@Repositories/ScopeRepository";
 
 @injectable()
 export class UnitOfWork {
   public user: UserRepository;
+  public role: RoleRepository;
+  public scope: ScopeRepository;
 
   constructor(context: PrismaClient) {
     this.user = new UserRepository(context);
+    this.role = new RoleRepository(context);
+    this.scope = new ScopeRepository(context);
   };
 };
