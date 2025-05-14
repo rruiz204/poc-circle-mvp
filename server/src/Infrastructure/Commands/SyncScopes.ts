4c74e8a2954604a53bfb26b7fc73cc9403298e15
roney ruiz <roneyruizrojas@gmail.com>
2025-05-13 21:09:48 -0600

    feat(server): create sync scopes command

---
+++ server/src/Infrastructure/Commands/SyncScopes.ts
@@ -0,0 +1,37 @@
+import { RBAC } from "@Security/RBAC";
+import type { Command } from "./Command";
+import { Context } from "@Database/Core/Context";
+import { Inversify } from "@Containers/Inversify";
+import { UnitOfWork } from "@Database/Core/UnitOfWork";
+
+export class SyncScopes implements Command {
+  constructor(private uow: UnitOfWork) {};
+
+  public async run(): Promise<void> {
+    await Context.roleScope.deleteMany();
+
+    for (const role in RBAC) {
+      const existRole = await this.uow.role.findByName(role);
+      if (!existRole) return;
+
+      const scopes = RBAC[role];
+      
+      for (const scope of scopes) {
+        const existScope = await this.uow.scope.findByName(scope);
+        if (!existScope) return;
+
+        await Context.roleScope.create({
+          data: {
+            roleId: existRole.id,
+            scopeId: existScope.id,
+          },
+        });
+      };
+    };
+
+    console.log("=== scopes synchronized ===");
+  };
+};
+
+const command = new SyncScopes(Inversify.get(UnitOfWork));
+await command.run();
