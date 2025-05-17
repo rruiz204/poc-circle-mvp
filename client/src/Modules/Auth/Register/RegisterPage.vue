6ea18b94c1338942f48453b7c2064cb809340495
roney ruiz <roneyruizrojas@gmail.com>
2025-05-16 21:29:47 -0600

    feat(client): create register page

---
+++ client/src/Modules/Auth/Register/RegisterPage.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import RegisterForm from './RegisterForm.vue';
+</script>
+
+<template>
+  <div class="min-h-screen flex flex-col justify-center items-center">
+    <div class="mb-5">
+      <h1 class="text-2xl text-center">Welcome, login to continue</h1>
+    </div>
+
+    <RegisterForm />
+  </div>
+</template>
