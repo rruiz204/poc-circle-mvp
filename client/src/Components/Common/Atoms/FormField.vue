e08f915acb8dc5b66e138d635b43e60791e5decd
roney ruiz <roneyruizrojas@gmail.com>
2025-05-17 23:25:14 -0600

    feat(client): create form field atom

---
+++ client/src/Components/Common/Atoms/FormField.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import Message from "primevue/message";
+
+defineProps<{
+  form?: any;
+  name: string;
+  label: string;
+}>();
+</script>
+
+<template>
+  <div class="flex flex-col gap-1">
+    <label :for="name" class="text-zinc-400">{{ label }}</label>
+    <slot></slot>
+    <Message v-if="form?.invalid" severity="error" size="small" variant="simple">
+      {{ form.error?.message }}
+    </Message>
+  </div>
+</template>
