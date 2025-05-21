c585162984c6b3194e5158020e6b5b60399d30fc
roney ruiz <roneyruizrojas@gmail.com>
2025-05-20 22:03:48 -0600

    feat(client): configure pinia

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,13 +1,16 @@
 import "./style.css";
 import App from "./App.vue";
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 
 import { Theme } from "./theme";
 import PrimeVue from "primevue/config";
 import { Router } from "@Routes/Router";
 
 const app = createApp(App);
+const pinia = createPinia();
 
+app.use(pinia);
 app.use(Router);
 app.use(PrimeVue, { theme: Theme });
 
