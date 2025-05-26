70c3f38cd4d71fadbda676bf1e4efdffe500be07
roney ruiz <roneyruizrojas@gmail.com>
2025-05-26 10:11:41 -0600

    feat(server): create repository abstractions

--- server/src/Infrastructure/Database/Common/UnitOfWork.ts
+++ server/src/Infrastructure/Database/Common/UnitOfWork.ts
@@ -1,15 +1,19 @@
-import { PrismaClient } from "generated/prisma";
 import { injectable } from "inversify";
+import { PrismaClient } from "generated/prisma";
 
 import { UserRepository } from "@Repositories/UserRepository";
 import { RoleRepository } from "@Repositories/RoleRepository";
 import { ScopeRepository } from "@Repositories/ScopeRepository";
 
+import type { IUserRepository } from "@Repositories/IUserRepository";
+import type { IRoleRepository } from "@Repositories/IRoleRepository";
+import type { IScopeRepository } from "@Repositories/IScopeRepository";
+
 @injectable()
 export class UnitOfWork {
-  public user: UserRepository;
-  public role: RoleRepository;
-  public scope: ScopeRepository;
+  public user:IUserRepository;
+  public role: IRoleRepository;
+  public scope: IScopeRepository;
 
   constructor(context: PrismaClient) {
     this.user = new UserRepository(context);
