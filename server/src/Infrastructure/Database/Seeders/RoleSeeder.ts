04a60a58d19db959ba2abf152ce96cf717a1ea82
roney ruiz <roneyruizrojas@gmail.com>
2025-05-26 10:12:16 -0600

    chore(server): remove roles and scopes data

--- server/src/Infrastructure/Database/Seeders/RoleSeeder.ts
+++ server/src/Infrastructure/Database/Seeders/RoleSeeder.ts
@@ -1,7 +1,6 @@
-import { Seeder } from "./Seeder";
+import { Role } from "@Models/Role";
 import { PrismaClient } from "generated/prisma";
-import type { Role as Model } from "@Models/Role";
-import { Roles } from "@Database/Common/Data/Roles";
+import { Seeder } from "@Database/Common/Seeder";
 
 export class RoleSeeder extends Seeder {
   constructor(context: PrismaClient) {
@@ -13,7 +12,7 @@ export class RoleSeeder extends Seeder {
     await this.remove(Roles);
   };
 
-  private async upsert(roles: Model.CreateParams[]): Promise<void> {
+  private async upsert(roles: Role.CreateParams[]): Promise<void> {
     for (const role of roles) {
       await this.context.role.upsert({
         where: { name: role.name },
@@ -23,10 +22,33 @@ export class RoleSeeder extends Seeder {
     };
   };
 
-  private async remove(roles: Model.CreateParams[]): Promise<void> {
+  private async remove(roles: Role.CreateParams[]): Promise<void> {
     const names = roles.map(role => role.name);
     await this.context.role.deleteMany({
       where: { name: { notIn: names } }
     });
   };
 };
+
+const Roles: Role.CreateParams[] = [
+  {
+    name: Role.Enum.ADMIN,
+    description: "Full access to all system features, including user and permission management",
+  },
+  {
+    name: Role.Enum.LEADER,
+    description: "Oversees team members and tasks within specific projects; can assign and update tasks",
+  },
+  {
+    name: Role.Enum.MANAGER,
+    description: "Manages entire projects, tasks, and team workflows; broader access than leader",
+  },
+  {
+    name: Role.Enum.DEVELOPER,
+    description: "Works on assigned tasks; can update status and add comments",
+  },
+  {
+    name: Role.Enum.STAKEHOLDER,
+    description: "Can view project progress and possibly comment; no editing permissions",
+  },
+];
