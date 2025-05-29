332d283c31f213174ae8ae58a02ef9cace404b83
roney ruiz <roneyruizrojas@gmail.com>
2025-05-28 22:02:30 -0600

    feat(server): create entrypoint script

---
+++ server/scripts/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+# Running database migrations...
+bun run prisma:migrate:deploy
+
+# Generating Prisma client..
+bun run prisma:generate
+
+# Seeding database...
+bun run prisma:seed
+
+# Running commands...
+bun run command:sync:rbac
+
+# Starting application...
+exec bun run prod
