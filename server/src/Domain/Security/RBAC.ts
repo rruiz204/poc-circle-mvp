27b0c2f05e49e25d4d9624bbf08c0dd58a5a84dd
roney ruiz <roneyruizrojas@gmail.com>
2025-06-03 22:07:11 -0600

    chore(server): remove old roles and scopes

--- server/src/Domain/Security/RBAC.ts
+++ server/src/Domain/Security/RBAC.ts
@@ -1,30 +1,4 @@
 import { Role } from "@Models/Role";
 import { Scope } from "@Models/Scope";
 
-export const RBAC: Record<string, Scope.Enum[]> = {
-  [Role.Enum.ADMIN]: [
-    Scope.Enum.ALL_TASKS,
-  ],
-  [Role.Enum.LEADER]: [
-    Scope.Enum.VIEW_TASKS,
-    Scope.Enum.EDIT_TASKS,
-    Scope.Enum.STATUS_TASKS,
-    Scope.Enum.ASSING_TASKS,
-    Scope.Enum.COMMENT_TASKS,
-  ],
-  [Role.Enum.MANAGER]: [
-    Scope.Enum.VIEW_TASKS,
-    Scope.Enum.CREATE_TASKS,
-    Scope.Enum.EDIT_TASKS,
-    Scope.Enum.STATUS_TASKS,
-    Scope.Enum.COMMENT_TASKS,
-  ],
-  [Role.Enum.DEVELOPER]: [
-    Scope.Enum.VIEW_TASKS,
-    Scope.Enum.STATUS_TASKS,
-    Scope.Enum.COMMENT_TASKS,
-  ],
-  [Role.Enum.STAKEHOLDER]: [
-    Scope.Enum.VIEW_TASKS,
-  ],
-};
+export const RBAC: Record<string, Scope.Enum[]> = {};
