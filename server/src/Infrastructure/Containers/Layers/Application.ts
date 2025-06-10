605324494ce790ce288655f013fec9d1779dc4cc
roney ruiz <roneyruizrojas@gmail.com>
2025-06-09 21:34:47 -0600

    feat(server): create list circles endpoint

--- server/src/Infrastructure/Containers/Layers/Application.ts
+++ server/src/Infrastructure/Containers/Layers/Application.ts
@@ -3,11 +3,13 @@ import { ContainerModule } from "inversify";
 import { EmailLoginUseCase } from "@UseCases/Auth/EmailLogin/EmailLoginUseCase";
 import { EmailRegisterUseCase } from "@UseCases/Auth/EmailRegister/EmailRegisterUseCase";
 
+import { ListCirclesUseCase } from "@UseCases/Circles/ListCircles/ListCirclesUseCase";
 import { CreateCircleUseCase } from "@UseCases/Circles/CreateCircle/CreateCircleUseCase";
 
 export const Application = new ContainerModule((opts) => {
   opts.bind(EmailLoginUseCase).toSelf();
   opts.bind(EmailRegisterUseCase).toSelf();
 
+  opts.bind(ListCirclesUseCase).toSelf();
   opts.bind(CreateCircleUseCase).toSelf();
 });
--- server/src/Presentation/Controllers/CircleController.ts
+++ server/src/Presentation/Controllers/CircleController.ts
@@ -1,14 +1,22 @@
 import { inject, injectable } from "inversify";
 import type { Request, Response } from "express";
 
+import { ListCirclesUseCase } from "@UseCases/Circles/ListCircles/ListCirclesUseCase";
 import { CreateCircleUseCase } from "@UseCases/Circles/CreateCircle/CreateCircleUseCase";
 
 @injectable()
 export class CircleController {
   constructor(
+    @inject(ListCirclesUseCase) private readonly listCirclesUseCase: ListCirclesUseCase,
     @inject(CreateCircleUseCase) private readonly createCirleUseCase: CreateCircleUseCase,
   ) {
+    this.list = this.list.bind(this);
     this.create = this.create.bind(this);
+  };
+
+  public async list(req: Request, res: Response): Promise<void> {
+    const response = await this.listCirclesUseCase.execute({ user: req.user.id });
+    res.status(200).json(response);
   };
 
   public async create(req: Request, res: Response): Promise<void> {
