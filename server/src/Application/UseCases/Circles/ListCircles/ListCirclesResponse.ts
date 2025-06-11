49996c293ee2ac01290860bd070d29fcd19285c7
roney ruiz <roneyruizrojas@gmail.com>
2025-06-10 22:36:39 -0600

    feat(server): build my circle spec and implement it

--- server/src/Application/UseCases/Circles/ListCircles/ListCirclesResponse.ts
+++ server/src/Application/UseCases/Circles/ListCircles/ListCirclesResponse.ts
@@ -1,3 +1,6 @@
+import type { UserDTO } from "@DTOs/UserDTO";
 import type { CircleDTO } from "@DTOs/CircleDTO";
 
-export interface ListCirclesResponse extends CircleDTO {};
+export interface ListCirclesResponse extends CircleDTO {
+  members: UserDTO[];
+};
--- server/src/Application/UseCases/Circles/ListCircles/ListCirclesUseCase.ts
+++ server/src/Application/UseCases/Circles/ListCircles/ListCirclesUseCase.ts
@@ -4,26 +4,23 @@ import type { ListCirclesResponse } from "./ListCirclesResponse";
 
 import { inject, injectable } from "inversify";
 import { UnitOfWork } from "@Database/Common/UnitOfWork";
+import { MyCirclesSpec } from "@Specs/Circles/MyCirclesSpec";
 
 @injectable()
 export class ListCirclesUseCase implements UseCase<ListCirclesQuery, ListCirclesResponse[]> {
   constructor(@inject(UnitOfWork) private readonly uow: UnitOfWork) {};
 
   public async execute(query: ListCirclesQuery): Promise<ListCirclesResponse[]> {
-    const circles = await this.uow.circle.list({
-      members: {
-        every: {
-          userId: {
-            equals: query.user
-          },
-        },
-      },
-    });
+    const spec = new MyCirclesSpec({ userId: query.user });
+    const circles = await this.uow.circle.list(spec);
+
+    // console.log(circles[0].members[0].user);
 
     return circles.map((c) => ({
       id: c.id,
       name: c.name,
       description: c.description,
+      members: [],
     }));
   };
 };
