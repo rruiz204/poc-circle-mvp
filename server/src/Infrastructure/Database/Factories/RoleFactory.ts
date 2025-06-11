0dbae3eee4e954bcf393f78c58933053bc19b354
roney ruiz <roneyruizrojas@gmail.com>
2025-06-10 23:00:17 -0600

    fix(server): role factory method naming

--- server/src/Infrastructure/Database/Factories/RoleFactory.ts
+++ server/src/Infrastructure/Database/Factories/RoleFactory.ts
@@ -2,7 +2,7 @@ import { faker } from "@faker-js/faker";
 import type { Role } from "@Models/Role";
 
 export class RoleFactory {
-  public static async buidl(role: Role.Optional): Promise<Role.Entity> {
+  public static async build(role: Role.Optional): Promise<Role.Entity> {
     return {
       id: role.id || faker.number.int({ min: 1, max: 100 }),
       name: role.name || faker.helpers.arrayElement(["admin", "member", "owner", "guest"]),
