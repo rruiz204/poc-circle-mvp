2259faacaeedb9387649cd8da18a386ee463607a
roney ruiz <roneyruizrojas@gmail.com>
2025-06-12 18:17:16 -0600

    refac(server): create abstract class for specs

--- server/src/Domain/Specs/Specification.ts
+++ server/src/Domain/Specs/Specification.ts
@@ -1,4 +1,19 @@
-export interface Specification<TWhere, TInclude> {
-  toQuery?(): TWhere;
-  toInclude?(): TInclude;
+export interface Specification<TWhere, TInclude, TUnique> {
+  toWhere(): TWhere;
+  toUnique(): TUnique;
+  toInclude(): TInclude;
+};
+
+export abstract class BaseSpec<TWhere, TInclude, TUnique> implements Specification<TWhere, TInclude, TUnique> {
+  public toWhere(): TWhere {
+    return {} as TWhere;
+  };
+
+  public toUnique(): TUnique {
+    return {} as TUnique;
+  };
+
+  public toInclude(): TInclude {
+    return {} as TInclude;
+  };
 };
