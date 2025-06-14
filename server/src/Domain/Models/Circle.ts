23d46281a41b2f3841dbda088c0d1a446f155901
roney ruiz <roneyruizrojas@gmail.com>
2025-06-13 22:14:52 -0600

    refac(server): add new props to models

--- server/src/Domain/Models/Circle.ts
+++ server/src/Domain/Models/Circle.ts
@@ -1,17 +1,21 @@
 import { Prisma } from "generated/prisma";
+import { BaseSpec } from "@Specs/Specification";
 import type { Circle as Model } from "generated/prisma";
-import type { Specification } from "@Specs/Specification";
 
 export namespace Circle {
   export type Entity = Model;
-  export type Records = Model[];
   export type Nullable = Model | null;
-  export type Optional = Partial<Model>;
 
-  export type WhereParams = Prisma.CircleWhereInput;
   export type CreateParams = Prisma.CircleCreateInput;
   export type UpdateParams = Prisma.CircleUpdateInput;
 
   export type IncludeParams = Prisma.CircleInclude;
-  export type Spec = Specification<WhereParams, IncludeParams>;
+  export type WhereParams = Prisma.CircleWhereInput;
+  export type UniqueParams = Prisma.CircleWhereUniqueInput;
+
+  export class Spec extends BaseSpec<WhereParams, IncludeParams, UniqueParams> {};
+
+  export type WithMembers = Prisma.CircleGetPayload<{
+    include: { members: { include: { user: true } } }
+  }>;
 };
--- server/src/Domain/Models/Member.ts
+++ server/src/Domain/Models/Member.ts
@@ -1,20 +1,8 @@
 import { Prisma } from "generated/prisma";
-import type { User } from "./User";
-import type { Role } from "./Role";
 import type { Member as Model } from "generated/prisma";
 
 export namespace Member {
   export type Entity = Model;
-  export type Records = Model[];
-  export type Nullable = Model | null;
-  export type Optional = Partial<Model>;
-
-  export type WithUser = Entity & { user: User.Entity };
-  export type WithRole = Entity & { role: Role.Entity };
-
-  export type WhereParams = Prisma.MemberWhereInput;
-  export type CreateParams = Prisma.MemberCreateInput;
-  export type UpdateParams = Prisma.MemberUpdateInput;
 
   export type UncheckedParams = Prisma.MemberUncheckedCreateInput;
   export type DeleteParams = Prisma.MemberUserIdCircleIdCompoundUniqueInput;
