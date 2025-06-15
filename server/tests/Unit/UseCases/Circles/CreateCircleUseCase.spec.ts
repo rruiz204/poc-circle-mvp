740009f93381a983b757b48a242468e3e0bd6c6b
roney ruiz <roneyruizrojas@gmail.com>
2025-06-14 21:05:42 -0600

    fix(server): replace uow by prisma in circles use cases

--- server/tests/Unit/UseCases/Circles/CreateCircleUseCase.spec.ts
+++ server/tests/Unit/UseCases/Circles/CreateCircleUseCase.spec.ts
@@ -1,33 +1,33 @@
 import { describe, expect, it, vi } from "vitest";
 
+import { PrismaClient } from "generated/prisma";
 import { Inversify } from "@Containers/Inversify";
-import { UnitOfWork } from "@Database/Common/UnitOfWork";
 import { LogicException } from "@Exceptions/LogicException";
 
 import { CreateCircleUseCaseFixture as Fixture } from "./CreateCircleUseCaseFixture";
 import { CreateCircleUseCase } from "@UseCases/Circles/CreateCircle/CreateCircleUseCase";
 
 describe("create circle use case", () => {
-  const uow = Inversify.get(UnitOfWork);
+  const prisma = Inversify.get(PrismaClient);
   const useCase = Inversify.get(CreateCircleUseCase);
 
   it("should create a new circle", async () => {
-    vi.spyOn(uow.circle, "findByName").mockResolvedValue(null);
-    vi.spyOn(uow.role, "findByName").mockResolvedValue(Fixture.role1);
-    vi.spyOn(uow.circle, "create").mockResolvedValue(Fixture.circle1);
+    vi.spyOn(prisma.circle, "findUnique").mockResolvedValue(null);
+    vi.spyOn(prisma.role, "findUnique").mockResolvedValue(Fixture.role1);
+    vi.spyOn(prisma.circle, "create").mockResolvedValue(Fixture.circle1);
 
     const response = await useCase.execute(Fixture.command);
     expect(response.name).toEqual(Fixture.circle1.name);
   });
 
   it("should throw Redundancy error", async () => {
-    vi.spyOn(uow.circle, "findByName").mockResolvedValue(Fixture.circle1);
+    vi.spyOn(prisma.circle, "findUnique").mockResolvedValue(Fixture.circle1);
     await expect(useCase.execute(Fixture.command)).rejects.toThrowError(LogicException.Redundancy);
   })
   
   it("should throw Not Found error", async () => {
-    vi.spyOn(uow.circle, "findByName").mockResolvedValue(null);
-    vi.spyOn(uow.role, "findByName").mockResolvedValue(null);
+    vi.spyOn(prisma.circle, "findUnique").mockResolvedValue(null);
+    vi.spyOn(prisma.role, "findUnique").mockResolvedValue(null);
     await expect(useCase.execute(Fixture.command)).rejects.toThrowError(LogicException.NotFound);
   });
 });
