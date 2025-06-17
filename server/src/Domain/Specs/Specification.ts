f4b6d934d078b57d0267edd04556565a97bd9e92
roney ruiz <roneyruizrojas@gmail.com>
2025-06-16 22:43:00 -0600

    refac(server): separate specification responsabilities

--- server/src/Domain/Specs/Specification.ts
+++ server/src/Domain/Specs/Specification.ts
@@ -1,19 +1,3 @@
-export interface Specification<TWhere, TInclude, TUnique> {
-  toWhere(): TWhere;
-  toUnique(): TUnique;
-  toInclude(): TInclude;
-};
-
-export abstract class BaseSpec<TWhere, TInclude, TUnique> implements Specification<TWhere, TInclude, TUnique> {
-  public toWhere(): TWhere {
-    return {} as TWhere;
-  };
-
-  public toUnique(): TUnique {
-    return {} as TUnique;
-  };
-
-  public toInclude(): TInclude {
-    return {} as TInclude;
-  };
+export interface Specification<TWhere> {
+  toWhere(): TWhere
 };
